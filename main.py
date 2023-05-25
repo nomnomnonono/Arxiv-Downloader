5da8dbdccbcedb5fb1b487121c71a70a4487ea67
nomnomnonono <murumuru1204@gmail.com>
2023-05-25 22:00:00 +0900

    refactoring

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ from scrape import save_config, scrape_paper
 
 
 def argparser() -> argparse.Namespace:
+    """
+    コマンドライン引数を受け取る
+
+    Returns:
+        argparse.Namespace: コマンドライン引数
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-c",
@@ -17,7 +23,13 @@ def argparser() -> argparse.Namespace:
     return args
 
 
-def main(args):
+def main(args: argparse.Namespace) -> None:
+    """
+    メインの実行関数
+
+    Args:
+        args (argparse.Namespace): コマンドライン引数
+    """
     config = OmegaConf.load(args.config)
     os.makedirs(os.path.join(config.base_dir, config.tag), exist_ok=True)
     config = scrape_paper(config)
--- scrape.py
+++ scrape.py
@@ -5,14 +5,27 @@ import time
 import arxiv
 import pandas as pd
 import yaml
+from omegaconf import OmegaConf
 from tqdm import tqdm
 
 
-def scrape_paper(config):
+def scrape_paper(config: OmegaConf) -> OmegaConf:
+    """
+    論文情報の取得を行う
+
+    Args:
+        config (OmegaConf): 設定ファイル
+
+    Returns:
+        OmegaConf: 更新後の設定ファイル
+    """
+
     if os.path.exists(os.path.join(config.base_dir, f"{config.tag}.csv")):
         df = pd.read_csv(os.path.join(config.base_dir, f"{config.tag}.csv"))
     else:
-        df = pd.DataFrame([], columns=["title", "abstract", "author", "year", "month", "link"])
+        df = pd.DataFrame(
+            [], columns=["title", "abstract", "author", "year", "month", "link"]
+        )
 
     idx = max(df.index) if len(df) > 0 else 0
 
@@ -45,13 +58,31 @@ def scrape_paper(config):
     return config
 
 
-def define_time_range(config):
+def define_time_range(config: OmegaConf) -> tuple[str, OmegaConf]:
+    """
+    いつから取得を行うかの情報取得と最終取得時刻の更新を行う
+
+    Args:
+        config (OmegaConf): 設定ファイル
+
+    Returns:
+        tuple[str, OmegaConf]: 取得開始時刻と更新後の設定ファイル
+    """
+
     now = "{:%Y%m%d%H%M%S}".format(datetime.datetime.now())
     time_query = f"submittedDate:[{config.before} TO {now}]"
     config["before"] = now
     return time_query, config
 
 
-def save_config(config, file_path):
+def save_config(config: OmegaConf, file_path: str) -> None:
+    """
+    設定ファイルを保存する
+
+    Args:
+        config (OmegaConf): 設定ファイル
+        file_path (str): 保存先ファイルパス
+    """
+
     with open(file_path, "w", encoding="utf-8") as f:
         yaml.dump(dict(config), f, allow_unicode=True, default_flow_style=False)
