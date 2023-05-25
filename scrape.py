a6d1096b6882337ba5f7e1b656585a71b0fee971
nomnomnonono <murumuru1204@gmail.com>
2023-05-25 21:53:17 +0900

    downloadを任意にする

--- scrape.py
+++ scrape.py
@@ -3,11 +3,19 @@ import os
 import time
 
 import arxiv
+import pandas as pd
 import yaml
 from tqdm import tqdm
 
 
 def scrape_paper(config):
+    if os.path.exists(os.path.join(config.base_dir, f"{config.tag}.csv")):
+        df = pd.read_csv(os.path.join(config.base_dir, f"{config.tag}.csv"))
+    else:
+        df = pd.DataFrame([], columns=["title", "abstract", "author", "year", "month", "link"])
+
+    idx = max(df.index) if len(df) > 0 else 0
+
     time_query, config = define_time_range(config)
     query = config.query + " AND " + time_query
     search = arxiv.Search(
@@ -17,8 +25,22 @@ def scrape_paper(config):
     )
 
     for result in tqdm(search.results()):
-        result.download_pdf(dirpath=os.path.join(config.base_dir, config.tag))
-        time.sleep(10)
+        year, month, _ = str(result.published).split(" ")[0].split("-")
+        df.loc[idx] = [
+            result.title.replace("'", "").replace('"', ""),
+            result.summary.replace("'", "").replace('"', ""),
+            str(result.authors[0]),
+            int(year),
+            int(month),
+            str(result.links[0]),
+        ]
+        if config.download_pdf:
+            result.download_pdf(dirpath=os.path.join(config.base_dir, config.tag))
+            time.sleep(10)
+
+        idx += 1
+
+    df.to_csv(os.path.join(config.base_dir, f"{config.tag}.csv"), index=False)
 
     return config
 
